f2d28b5fc9618dfc204a1efe31cb573796043154
johnnycabelin <132518223+johnnycabelin@users.noreply.github.com>
2024-03-18 20:07:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
 
+from domain.Archive import Arquive
 
 if __name__ == '__main__':
-    pass
+    file_name = "transparencia.csv"
+    arquivo = Archive(file_name)
+    if os.path.exists(file_name):
+        pass
+    else:
+        arquivo.create_archive()
+    
